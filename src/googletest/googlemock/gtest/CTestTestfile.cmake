d7add2ed6be92542dd94deb7f3833ef45c39286b
zcphoenix2016 <13564240769@139.com>
2016-10-20 15:16:09 +0800

    add built google test

---
+++ src/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chuanpiz/Downloads/googletest-master/googletest
+# Build directory: /home/chuanpiz/Downloads/googletest-master/build/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
